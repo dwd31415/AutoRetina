08b3c8613d41ba4a47075066ad7a8d3d6c11d40e
Adrian Dawid <adriandwd@gmail.com>
2014-06-22 12:20:44 +0200

    Added functions for switching resolution based on Device Model and Graphics Memory

---
+++ Assets/AutoRetina Toolkit/AutoRetina.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoRetina : MonoBehaviour {
+
+	public static void AutoSwitchResolution(iPhoneGeneration[] nonRetinaDevices)
+	{
+		foreach (iPhoneGeneration generation in nonRetinaDevices) {
+			if(iPhone.generation==generation)
+			{
+				if(Screen.width==(RetinaResolutions.iPadRetina.x|RetinaResolutions.iPadRetina.y))
+				{
+					Screen.SetResolution(1024,768);	
+				}
+				else{
+					Screen.SetResolution(480,320);
+				}
+			}
+		}
+	}
+	public static void AutoSwitchResolution(int minRetinaGraphicsMemory)
+	{
+		if(SystemInfo.graphicsMemorySize<minRetinaGraphicsMemory) {
+
+				if(Screen.width==(RetinaResolutions.iPadRetina.x|RetinaResolutions.iPadRetina.y))
+				{
+					Screen.SetResolution(1024,768);	
+				}
+				else{
+					Screen.SetResolution(480,320);
+				}
+
+		}
+	}
+
+
+}
---
+++ Assets/AutoRetina Toolkit/Helpers.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class RetinaResolutions{
+	public static Vector2 iPhone5Retina=new Vector2(1136,640);
+	public static Vector2 iPhone4SRetina=new Vector2(960,640);
+	public static Vector2 iPadRetina=new Vector2(2048,1536);
+}
+
+public enum AppleGPUS:short{
+	A3=1,A4,A5,A5X,A6,A6X,A7
+}
